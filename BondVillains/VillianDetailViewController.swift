008e1c9c8649f4ea2581daa2f08245b76ae58520
James Whitney <jamesdrums@mac.com>
2018-02-14 14:41:49 -0500

    feat: Adds nav controller and new ViewController.
    Navigation controller added to navigate between
    VillianDetailViewController and ViewController with TableView
    that contains BondVillains names and details.
    The VillianDetailViewController will show villian image and a
    label showing details about villian.

---
+++ BondVillains/VillianDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  VillianDetailViewController.swift
+//  BondVillains
+//
+//  Created by James Whitney on 2/14/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VillianDetailViewController: UIViewController {
+    
+}
